24c5c6e74072058a2fcb37a2ab2f8c527b05a947
JunDao <Tnze@Tnze.local>
2019-01-04 23:29:52 +0800

    Finish authenticate with useremail and password function.

---
+++ authenticate/authenticate_test.go
@@ -0,0 +1,29 @@
+package authenticate
+
+import "testing"
+import "encoding/json"
+
+func TestEncodingPayload(t *testing.T) {
+	j, err := json.Marshal(Payload{
+		Agent: Agent{
+			Name:    "Minecraft",
+			Version: 1,
+		},
+		UserName:    "mojang account name",
+		Password:    "mojang account password",
+		ClientToken: "client identifier",
+		RequestUser: true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(j))
+}
+
+func TestAuthenticate(t *testing.T) {
+	resp, err := Authenticate("email", "password")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(resp)
+}
